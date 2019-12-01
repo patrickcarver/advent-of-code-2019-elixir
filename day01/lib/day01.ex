b8d0e3594dfa9cd1db0e37732842757c83b321c4
Patrick Carver <patrickcarver@gmail.com>
2019-12-01 00:29:30 -0600

    add another Day 01 Part 2 with stream iteration

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -7,11 +7,17 @@ defmodule Day01 do
 
   def part2 do
     module_masses()
-    |> Stream.map(fn mass -> recurse(mass, 0) end)
-    |> Enum.sum
+    |> Stream.map(&recurse/1)
+    |> Enum.sum()
   end
 
-  def recurse(mass, acc) do
+  def part2_with_stream_iterate do
+    module_masses()
+    |> Stream.map(&iterate/1)
+    |> Enum.sum()
+  end
+
+  def recurse(mass, acc \\ 0) do
     fuel = fuel_required(mass)
 
     if fuel > 0 do
@@ -21,16 +27,24 @@ defmodule Day01 do
     end
   end
 
-  def module_masses do
-    "../data/input.txt"
-    |> Path.expand(__DIR__)
-    |> File.stream!()
-    |> Stream.map(fn line -> line |> String.trim_trailing |> String.to_integer end)
+  def iterate(mass) do
+    mass
+    |> Stream.iterate(&fuel_required/1)
+    |> Enum.take_while(& &1 > 0)
+    |> tl() # the list will have the initial mass
+    |> Enum.sum()
   end
 
   def fuel_required(mass) do
     mass
     |> div(3)
     |> Kernel.-(2)
+  end
+
+  def module_masses do
+    "../data/input.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(fn line -> line |> String.trim_trailing() |> String.to_integer() end)
   end
 end
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -3,18 +3,27 @@ defmodule Day01Test do
   doctest Day01
 
   test "fuel required" do
-    # assert Day01.hello() == :world
     assert Day01.fuel_required(12) == 2
     assert Day01.fuel_required(14) == 2
     assert Day01.fuel_required(1969) == 654
     assert Day01.fuel_required(100756) == 33583
   end
 
-  test "Day01 Part 1 result" do
-    assert Day01.part1 == 3392373
+  test "Day 01 Part 1 result" do
+    assert Day01.part1() == 3392373
   end
 
-  test "Day01 Part 2 result" do
-    assert Day01.part2 == 5085699
+  test "recursive fuel calculation" do
+    assert Day01.recurse(12, 0) == 2
+    assert Day01.recurse(1969, 0) == 966
+    assert Day01.recurse(100756, 0) == 50346
+  end
+
+  test "Day 01 Part 2 result" do
+    assert Day01.part2() == 5085699
+  end
+
+  test "Day 01 Part 2 with stream iteration result" do
+    assert Day01.part2_with_stream_iterate() == 5085699
   end
 end
