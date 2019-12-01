ecd8e4f19026bd4f0da21a0d59546419554f34ee
Patrick Carver <patrickcarver@gmail.com>
2019-11-30 23:51:18 -0600

    complete Day 01 Part 2

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -1,13 +1,32 @@
 defmodule Day01 do
   def part1 do
-    "../data/input.txt"
-    |> Path.expand(__DIR__)
-    |> File.stream!()
-    |> Stream.map(fn line -> line |> String.trim_trailing |> String.to_integer |> fuel_required end)
+    module_masses()
+    |> Stream.map(&fuel_required/1)
     |> Enum.sum
   end
 
+  def part2 do
+    module_masses()
+    |> Stream.map(fn mass -> recurse(mass, 0) end)
+    |> Enum.sum
+  end
 
+  def recurse(mass, acc) do
+    fuel = fuel_required(mass)
+
+    if fuel > 0 do
+      recurse(fuel, acc + fuel)
+    else
+      acc
+    end
+  end
+
+  def module_masses do
+    "../data/input.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(fn line -> line |> String.trim_trailing |> String.to_integer end)
+  end
 
   def fuel_required(mass) do
     mass
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -2,11 +2,19 @@ defmodule Day01Test do
   use ExUnit.Case
   doctest Day01
 
-  test "test fuel required" do
+  test "fuel required" do
     # assert Day01.hello() == :world
     assert Day01.fuel_required(12) == 2
     assert Day01.fuel_required(14) == 2
     assert Day01.fuel_required(1969) == 654
     assert Day01.fuel_required(100756) == 33583
+  end
+
+  test "Day01 Part 1 result" do
+    assert Day01.part1 == 3392373
+  end
+
+  test "Day01 Part 2 result" do
+    assert Day01.part2 == 5085699
   end
 end
