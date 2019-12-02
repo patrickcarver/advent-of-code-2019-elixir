dbdc99a560326e42c1334e74c09b8e1f6247ed10
Patrick Carver <patrickcarver@gmail.com>
2019-12-02 00:53:05 -0600

    complete Day 02 Part 1

--- day02/lib/day02.ex
+++ day02/lib/day02.ex
@@ -1,8 +1,70 @@
 defmodule Day02 do
-  def part1 do
+  def opcode_values(opcode) do
+    values = %{
+      1 => &Kernel.+/2,
+      2 => &Kernel.*/2,
+      99 => :halt
+    }
 
+    Map.get(values, opcode)
+  end
+
+  def loop(int_code, current_position) do
+    current_opcode = Map.get(int_code, current_position)
+    current_opcode_value = opcode_values(current_opcode)
+
+    case current_opcode_value do
+      :halt ->
+        int_code
+      _ ->
+        input1_position = Map.get(int_code, current_position + 1)
+        input2_position = Map.get(int_code, current_position + 2)
+        output_position = Map.get(int_code, current_position + 3)
+
+        input1 = Map.get(int_code, input1_position)
+        input2 = Map.get(int_code, input2_position)
+
+        result = apply(current_opcode_value, [input1, input2])
+        updated_int_code = Map.put(int_code, output_position, result)
+
+        updated_position = current_position + 4
+
+        loop(updated_int_code, updated_position)
+    end
+  end
+
+  def restore_1202_program_alarm(map) do
+    map
+    |> Map.put(1, 12)
+    |> Map.put(2, 2)
+  end
+
+  def part1 do
+    int_code = load() |> convert_to_map() |> restore_1202_program_alarm()
+    position = 0
+
+    loop(int_code, position) |> Map.get(0)
   end
 
   def part2 do
+  end
+
+
+
+  def load do
+    "../data/input.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Enum.map(&String.trim_trailing/1)
+    |> hd()
+  end
+
+  def convert_to_map(data) do
+    data
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index()
+    |> Enum.map(fn {value, key} -> {key, value} end)
+    |> Enum.into(%{})
   end
 end
--- day02/test/day02_test.exs
+++ day02/test/day02_test.exs
@@ -2,7 +2,39 @@ defmodule Day02Test do
   use ExUnit.Case
   doctest Day02
 
-  test "greets the world" do
-    assert Day02.hello() == :world
+  test "1,0,0,0,99" do
+    int_code = "1,0,0,0,99" |> Day02.convert_to_map()
+    position = 0
+
+    actual = Day02.loop(int_code, position)
+    expected = "2,0,0,0,99" |> Day02.convert_to_map()
+    assert actual == expected
+  end
+
+  test "2,3,0,3,99" do
+    int_code = "2,3,0,3,99" |> Day02.convert_to_map()
+    position = 0
+
+    actual = Day02.loop(int_code, position)
+    expected = "2,3,0,6,99" |> Day02.convert_to_map()
+    assert actual == expected
+  end
+
+  test "2,4,4,5,99,0"  do
+    int_code = "2,4,4,5,99,0" |> Day02.convert_to_map()
+    position = 0
+
+    actual = Day02.loop(int_code, position)
+    expected = "2,4,4,5,99,9801" |> Day02.convert_to_map()
+    assert actual == expected
+  end
+
+  test "1,1,1,4,99,5,6,0,99"  do
+    int_code = "1,1,1,4,99,5,6,0,99" |> Day02.convert_to_map()
+    position = 0
+
+    actual = Day02.loop(int_code, position)
+    expected = "30,1,1,4,2,5,6,0,99" |> Day02.convert_to_map()
+    assert actual == expected
   end
 end
