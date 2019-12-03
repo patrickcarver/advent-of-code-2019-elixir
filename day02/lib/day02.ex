422ed15d047af16d21de4da573d8958682490b1e
Patrick Carver <patrickcarver@gmail.com>
2019-12-02 19:31:00 -0600

    a whole bunch of clean up

--- day02/lib/day02.ex
+++ day02/lib/day02.ex
@@ -1,95 +1,42 @@
 defmodule Day02 do
-  def opcode_values(opcode) do
-    values = %{
-      1 => &Kernel.+/2,
-      2 => &Kernel.*/2,
-      99 => :halt
-    }
+  alias Day02.Program
 
-    Map.get(values, opcode)
-  end
-
-  def loop(int_code, current_position \\ 0)
-  def loop(int_code, current_position) do
-    current_opcode = Map.get(int_code, current_position)
-    current_opcode_value = opcode_values(current_opcode)
-
-    case current_opcode_value do
-      :halt ->
-        int_code
-      _ ->
-        input1_position = Map.get(int_code, current_position + 1)
-        input2_position = Map.get(int_code, current_position + 2)
-        output_position = Map.get(int_code, current_position + 3)
-
-        input1 = Map.get(int_code, input1_position)
-        input2 = Map.get(int_code, input2_position)
-
-        result = apply(current_opcode_value, [input1, input2])
-
-        updated_int_code = Map.put(int_code, output_position, result)
-        updated_position = current_position + 4
-
-        loop(updated_int_code, updated_position)
-    end
-  end
-
-  def change_input(map, noun, verb) do
-    map
-    |> Map.put(1, noun)
-    |> Map.put(2, verb)
-  end
-
-  @spec part1 :: any
   def part1 do
     load()
-    |> convert_to_map()
-    |> change_input(12, 2)
-    |> loop()
-    |> Map.get(0)
-  end
-
-  def find_pair_of_inputs_resulting_in_target(int_code, target, [{noun, verb} | rest]) do
-    result =
-      int_code
-      |> change_input(noun, verb)
-      |> loop()
-      |> Map.get(0)
-
-    case result do
-      ^target ->
-        100 * noun + verb
-      _ ->
-        find_pair_of_inputs_resulting_in_target(int_code, target, rest)
-    end
-  end
-
-  def inputs() do
-    for noun <- 0..99, verb <- 0..99, do: {noun, verb}
+    |> Program.initialize_memory()
+    |> restore_1202_program_alarm()
   end
 
   def part2 do
-    int_code = load() |> convert_to_map()
-    target = 19690720
-    inputs = inputs()
-
-    find_pair_of_inputs_resulting_in_target(int_code, target, inputs)
+    load()
+    |> Program.initialize_memory()
+    |> calculation_with_inputs_that_result_in_19690720(inputs())
   end
 
-  def load do
+  defp restore_1202_program_alarm(memory) do
+    Program.value_at_first_position(memory, 12, 2)
+  end
+
+  defp calculation_with_inputs_that_result_in_19690720(memory, [{noun, verb} | rest]) do
+    result = Program.value_at_first_position(memory, noun, verb)
+
+    case result do
+      19690720 ->
+        100 * noun + verb
+      _ ->
+        calculation_with_inputs_that_result_in_19690720(memory, rest)
+    end
+  end
+
+  defp inputs() do
+    for noun <- 0..99, verb <- 0..99, do: {noun, verb}
+  end
+
+  defp load do
     "../data/input.txt"
     |> Path.expand(__DIR__)
     |> File.stream!()
     |> Enum.map(&String.trim_trailing/1)
     |> hd()
-  end
-
-  def convert_to_map(data) do
-    data
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.with_index()
-    |> Enum.map(fn {value, key} -> {key, value} end)
-    |> Enum.into(%{})
   end
 end
--- day02/test/day02_test.exs
+++ day02/test/day02_test.exs
@@ -3,38 +3,26 @@ defmodule Day02Test do
   doctest Day02
 
   test "1,0,0,0,99" do
-    int_code = "1,0,0,0,99" |> Day02.convert_to_map()
-    position = 0
-
-    actual = Day02.loop(int_code, position)
-    expected = "2,0,0,0,99" |> Day02.convert_to_map()
+    actual = "1,0,0,0,99" |> Day02.Memory.initialize() |> Day02.Program.run()
+    expected = "2,0,0,0,99" |> Day02.Memory.initialize()
     assert actual == expected
   end
 
   test "2,3,0,3,99" do
-    int_code = "2,3,0,3,99" |> Day02.convert_to_map()
-    position = 0
-
-    actual = Day02.loop(int_code, position)
-    expected = "2,3,0,6,99" |> Day02.convert_to_map()
+    actual = "2,3,0,3,99" |> Day02.Memory.initialize() |> Day02.Program.run()
+    expected = "2,3,0,6,99" |> Day02.Memory.initialize()
     assert actual == expected
   end
 
-  test "2,4,4,5,99,0"  do
-    int_code = "2,4,4,5,99,0" |> Day02.convert_to_map()
-    position = 0
-
-    actual = Day02.loop(int_code, position)
-    expected = "2,4,4,5,99,9801" |> Day02.convert_to_map()
+  test "2,4,4,5,99,0" do
+    actual = "2,4,4,5,99,0" |> Day02.Memory.initialize() |> Day02.Program.run()
+    expected = "2,4,4,5,99,9801" |> Day02.Memory.initialize()
     assert actual == expected
   end
 
-  test "1,1,1,4,99,5,6,0,99"  do
-    int_code = "1,1,1,4,99,5,6,0,99" |> Day02.convert_to_map()
-    position = 0
-
-    actual = Day02.loop(int_code, position)
-    expected = "30,1,1,4,2,5,6,0,99" |> Day02.convert_to_map()
+  test "1,1,1,4,99,5,6,0,99" do
+    actual = "1,1,1,4,99,5,6,0,99" |> Day02.Memory.initialize() |> Day02.Program.run()
+    expected = "30,1,1,4,2,5,6,0,99" |> Day02.Memory.initialize()
     assert actual == expected
   end
 
