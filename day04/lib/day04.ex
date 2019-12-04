11dd41ed17482632b0dd99f6aa51d78a9fbaff37
Patrick Carver <patrickcarver@gmail.com>
2019-12-04 00:59:34 -0600

    complete Day 04 Part 2

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -2,8 +2,8 @@ defmodule Day04 do
   def part1 do
     125730..579381
     |> Stream.map(&Integer.digits/1)
-    |> Stream.reject(fn digits ->
-      digits == Enum.dedup(digits)
+    |> Stream.filter(fn digits ->
+      digits != Enum.dedup(digits)
     end)
     |> Stream.filter(fn digits ->
       digits == Enum.sort(digits)
@@ -13,6 +13,25 @@ defmodule Day04 do
   end
 
   def part2 do
-    :todo
+    125730..579381
+    |> Stream.map(&Integer.digits/1)
+    |> Stream.filter(fn digits ->
+      process(digits)
+    end)
+    |> Stream.filter(fn digits ->
+      digits == Enum.sort(digits)
+    end)
+    |> Enum.to_list()
+    |> length
+  end
+
+  def process(digits) do
+    Enum.reduce(digits, [{0,0}], fn digit, [{last_digit, count} | rest] = acc ->
+      if digit == last_digit do
+        [{digit, count + 1} | rest]
+      else
+        [{digit, 1} | acc]
+      end
+    end) |> Enum.any?(fn {_digit, count} -> count == 2 end)
   end
 end
