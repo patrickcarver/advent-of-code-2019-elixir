29d7e24f8bf0318013c6bf8c3928f31139d80aad
Patrick Carver <patrickcarver@gmail.com>
2019-12-04 01:16:45 -0600

    refactor Day 04

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -1,31 +1,30 @@
 defmodule Day04 do
   def part1 do
-    125730..579381
-    |> Stream.map(&Integer.digits/1)
-    |> Stream.filter(fn digits ->
-      digits != Enum.dedup(digits)
-    end)
-    |> Stream.filter(fn digits ->
-      digits == Enum.sort(digits)
-    end)
-    |> Enum.to_list()
-    |> length
+    process(125730..579381, &at_least_two_adjacent_matching_digits/1)
   end
 
   def part2 do
-    125730..579381
+    process(125730..579381, &two_adjacent_matching_digits_not_in_larger_match/1)
+  end
+
+  defp process(range, matching_rule) do
+    range
     |> Stream.map(&Integer.digits/1)
-    |> Stream.filter(fn digits ->
-      process(digits)
-    end)
-    |> Stream.filter(fn digits ->
-      digits == Enum.sort(digits)
-    end)
+    |> Stream.filter(matching_rule)
+    |> Stream.filter(&in_ascending_order/1)
     |> Enum.to_list()
     |> length
   end
 
-  def process(digits) do
+  def in_ascending_order(digits) do
+    Enum.sort(digits) == digits
+  end
+
+  def at_least_two_adjacent_matching_digits(digits) do
+    digits != Enum.dedup(digits)
+  end
+
+  def two_adjacent_matching_digits_not_in_larger_match(digits) do
     Enum.reduce(digits, [{0,0}], fn digit, [{last_digit, count} | rest] = acc ->
       if digit == last_digit do
         [{digit, count + 1} | rest]
--- day04/test/day04_test.exs
+++ day04/test/day04_test.exs
@@ -2,7 +2,11 @@ defmodule Day04Test do
   use ExUnit.Case
   doctest Day04
 
-  test "greets the world" do
-    assert Day04.hello() == :world
+  test "part1" do
+    assert Day04.part1 == 2081
+  end
+
+  test "part2" do
+    assert Day04.part2 == 1411
   end
 end
