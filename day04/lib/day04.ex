034695b9d964af4ded848a7633347dfbf6b11271
Patrick Carver <patrickcarver@gmail.com>
2019-12-03 23:50:08 -0600

    complete Day 04 Part 1

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -1,18 +1,18 @@
 defmodule Day04 do
-  @moduledoc """
-  Documentation for Day04.
-  """
+  def part1 do
+    125730..579381
+    |> Stream.map(&Integer.digits/1)
+    |> Stream.reject(fn digits ->
+      digits == Enum.dedup(digits)
+    end)
+    |> Stream.filter(fn digits ->
+      digits == Enum.sort(digits)
+    end)
+    |> Enum.to_list()
+    |> length
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day04.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def part2 do
+    :todo
   end
 end
