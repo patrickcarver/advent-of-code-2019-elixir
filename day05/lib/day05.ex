b957e29ebf2f3f7c2d0856d08fd5b54cb69afa44
Patrick Carver <patrickcarver@gmail.com>
2019-12-05 16:24:03 -0600

    remote day02 code

--- day05/lib/day05.ex
+++ day05/lib/day05.ex
@@ -1,9 +1,19 @@
 defmodule Day05 do
   alias Day05.Program
   def part1 do
+    load()
+    |> Program.initialize_memory()
   end
 
   def part2 do
     :todo
+  end
+
+  defp load do
+    "../data/input.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Enum.map(&String.trim_trailing/1)
+    |> hd()
   end
 end
