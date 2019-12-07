eab61b9757276c3f7a9ec0894076915a944629ed
Patrick Carver <patrickcarver@gmail.com>
2019-12-07 08:25:59 -0600

    complete Day 06 Part 1

---
+++ day06/.elixir_ls/build/test/lib/day06/ebin/day06.app
@@ -0,0 +1,6 @@
+{application,day06,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"day06"},
+              {modules,['Elixir.Day06']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- day06/lib/day06.ex
+++ day06/lib/day06.ex
@@ -1,18 +1,44 @@
 defmodule Day06 do
-  @moduledoc """
-  Documentation for Day06.
-  """
+  def part1 do
+    "../data/input.txt"
+    |> load()
+    |> build_data()
+    |> total_orbits()
+  end
 
-  @doc """
-  Hello world.
+  def total_orbits(%{list: list, map: map}) do
+    Enum.reduce(list, 0, fn object, total ->
+      count(object, map, total)
+    end)
+  end
 
-  ## Examples
+  def count("COM", _map, total) do
+    total
+  end
 
-      iex> Day06.hello()
-      :world
+  def count(object, map, total) do
+    left = Map.get(map, object)
+    count(left, map, total + 1)
+  end
 
-  """
-  def hello do
-    :world
+  def build_data(stream) do
+    stream
+    |> Stream.map(fn line -> String.split(line, ")") end)
+    |> Enum.reduce(%{list: [], map: %{}}, fn [left, right], %{list: list, map: map} ->
+      list = [right | list]
+      map = Map.put(map, right, left)
+      %{list: list, map: map}
+    end)
+  end
+
+  def part2 do
+    :noop
+  end
+
+  def load(file_name) do
+    file_name
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
   end
 end
