cc36244c2567a65d02cbaac55e0a2963a29267ab
Patrick Carver <patrickcarver@gmail.com>
2020-01-21 22:19:33 -0600

    Day08: finish part 1

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -3,6 +3,30 @@ defmodule Day08 do
     "../priv/input.txt"
     |> load()
     |> layers(%{width: 25, height: 6})
+    |> fewest_zeros()
+    |> multiply_ones_and_twos()
+  end
+
+  def part2() do
+    "../priv/input.txt"
+    |> load()
+    |> layers(%{width: 25, height: 6})
+  end
+
+  def fewest_zeros(layers) do
+    layers
+    |> Enum.map(fn layer ->
+      layer
+      |> List.flatten()
+      |> Enum.reduce(%{}, fn digit, acc ->
+        Map.update(acc, digit, 1, & &1 + 1)
+      end)
+    end)
+    |> Enum.min_by(fn %{"0" => zeros} -> zeros end)
+  end
+
+  def multiply_ones_and_twos(%{"1" => ones, "2" => twos}) do
+    ones * twos
   end
 
   def layers(input, %{width: width, height: height}) do
