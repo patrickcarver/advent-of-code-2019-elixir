2762b4201039b33bcc10d20039ff6cef5715ac17
Patrick Carver <patrickcarver@gmail.com>
2020-01-25 12:56:57 -0600

    Finish redoing Day08

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -1,46 +1,79 @@
 defmodule Day08 do
-  def part1() do
-    "../priv/input.txt"
-    |> load()
-    |> layers(%{width: 25, height: 6})
+  def run(:part1) do
+    starting_data() |> part1()
+  end
+
+  def run(:part2) do
+    starting_data() |> part2() |> IO.puts()
+  end
+
+  def part1(data) do
+    data
+    |> layers()
     |> fewest_zeros()
-    |> multiply_ones_and_twos()
+    |> nonzero_product()
   end
 
-  def part2() do
-    "../priv/input.txt"
-    |> load()
-    |> layers(%{width: 25, height: 6})
+  def part2(data) do
+    data
+    |> layers()
+    |> overlay()
+    |> format()
   end
 
-  def fewest_zeros(layers) do
-    layers
-    |> Enum.map(fn layer ->
-      layer
-      |> List.flatten()
-      |> Enum.reduce(%{}, fn digit, acc ->
-        Map.update(acc, digit, 1, & &1 + 1)
+  def starting_data() do
+    %{
+      pixels: pixels_from_file("priv/input.txt"),
+      width: 25,
+      height: 6
+    }
+  end
+
+  defp layers(%{pixels: pixels, width: width, height: height}) do
+    layers = Enum.chunk_every(pixels, height * width)
+    %{layers: layers, width: width}
+  end
+
+  defp overlay(%{layers: layers, width: width}) do
+    layer =
+      Enum.reduce(layers, fn bottom, top ->
+        bottom
+        |> Enum.zip(top)
+        |> Enum.map(fn
+          {_b, "0"} -> "0"
+          {_b, "1"} -> "1"
+          {b,  "2"} -> b
+        end)
       end)
+
+    %{layer: layer, width: width}
+  end
+
+  defp format(%{layer: layer, width: width}) do
+    layer
+    |> Enum.map(fn
+      "0" -> IO.ANSI.black() <> "█"
+      "1" -> IO.ANSI.white() <> "█"
     end)
-    |> Enum.min_by(fn %{"0" => zeros} -> zeros end)
-  end
-
-  def multiply_ones_and_twos(%{"1" => ones, "2" => twos}) do
-    ones * twos
-  end
-
-  def layers(input, %{width: width, height: height}) do
-    input
-    |> String.codepoints()
     |> Enum.chunk_every(width)
-    |> Enum.chunk_every(height)
+    |> Enum.join("\n")
   end
 
-  def load(file_name) do
+  defp fewest_zeros(%{layers: layers}) do
+    Enum.min_by(layers, fn pixels -> count(pixels, "0") end)
+  end
+
+  defp nonzero_product(pixels) do
+    count(pixels, "1") * count(pixels, "2")
+  end
+
+  defp count(pixels, value) do
+    Enum.count(pixels, &(&1 == value))
+  end
+
+  defp pixels_from_file(file_name) do
     file_name
-    |> Path.expand(__DIR__)
-    |> File.stream!()
-    |> Enum.map(&String.trim_trailing/1)
-    |> hd()
+    |> File.read!()
+    |> String.codepoints()
   end
 end
