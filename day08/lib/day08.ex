e279c9a5b405b5600a56edd5d576c99541c38cd9
Patrick Carver <patrickcarver@gmail.com>
2020-01-27 17:34:01 -0600

    reorganize code

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -1,10 +1,13 @@
 defmodule Day08 do
   def run(:part1) do
-    starting_data() |> part1()
+    starting_data()
+    |> part1()
   end
 
   def run(:part2) do
-    starting_data() |> part2() |> IO.puts()
+    starting_data()
+    |> part2()
+    |> IO.puts()
   end
 
   def part1(data) do
@@ -29,12 +32,30 @@ defmodule Day08 do
     }
   end
 
-  defp layers(%{pixels: pixels, width: width, height: height}) do
+  def pixels_from_file(file_name) do
+    file_name
+    |> File.read!()
+    |> String.codepoints()
+  end
+
+  def layers(%{pixels: pixels, width: width, height: height}) do
     layers = Enum.chunk_every(pixels, height * width)
     %{layers: layers, width: width}
   end
 
-  defp overlay(%{layers: layers, width: width}) do
+  def fewest_zeros(%{layers: layers}) do
+    Enum.min_by(layers, &(count(&1, "0")))
+  end
+
+  def nonzero_product(pixels) do
+    count(pixels, "1") * count(pixels, "2")
+  end
+
+  def count(pixels, value) do
+    Enum.count(pixels, &(&1 == value))
+  end
+
+  def overlay(%{layers: layers, width: width}) do
     layer =
       Enum.reduce(layers, fn bottom, top ->
         bottom
@@ -48,44 +69,13 @@ defmodule Day08 do
     %{layer: layer, width: width}
   end
 
-  defp format(%{layer: layer, width: width}) do
-    translate_pixel = select_translate(IO.ANSI.enabled?())
-
+  def format(%{layer: layer, width: width}) do
     layer
-    |> Enum.map(translate_pixel)
-    |> Enum.chunk_every(width)
-    |> Enum.join("\n")
-  end
-
-  defp select_translate(true) do
-    fn
-      "0" -> IO.ANSI.black() <> "█"
-      "1" -> IO.ANSI.white() <> "█"
-    end
-  end
-
-  defp select_translate(false) do
-    fn
+    |> Enum.map(fn
       "0" -> " "
       "1" -> "█"
-    end
-  end
-
-  defp fewest_zeros(%{layers: layers}) do
-    Enum.min_by(layers, &(count(&1, "0")))
-  end
-
-  defp nonzero_product(pixels) do
-    count(pixels, "1") * count(pixels, "2")
-  end
-
-  defp count(pixels, value) do
-    Enum.count(pixels, &(&1 == value))
-  end
-
-  defp pixels_from_file(file_name) do
-    file_name
-    |> File.read!()
-    |> String.codepoints()
+    end)
+    |> Enum.chunk_every(width)
+    |> Enum.join("\n")
   end
 end
